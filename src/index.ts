9095056895500401bebcee625791c337babad931
krytoy_dobrinya <mixailovuyriy@gmail.com>
2025-03-09 14:50:34 +0300

    modified:   package-lock.json 	modified:   package.json 	renamed:    index.ts -> src/index.ts 	new file:   src/views/index.ejs 	new file:   src/views/layout.ejs 	new file:   styles/style.css 	modified:   tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req: Request, res: Response) => {
+app.set("view engine", "ejs")
+
+app.post('/ejs-pages', (req: Request, res: Response) => {
+  res.render('layout', {message: "My message!321", title: "My page", content: "index"})
+})
+
+app.post('/', (req: Request, res: Response) => {
   console.log(req.body)
   console.log(req.headers)
   res.send('Hello World!123')
